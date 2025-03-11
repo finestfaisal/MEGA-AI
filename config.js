7ed109a003067de6f7272b2c97388d47f87adacc
finestfaisal <finestfaisal@gmail.com>
2025-03-11 13:35:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '255629028037';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '255629028037' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'zRgmWQIb#e4W1ng5WQRc4W6T9jm0cfv5gpl08p-BMlKLd4WfCn7s' ;  // put your session id here
 
 global.mods = []
 global.prems = []
